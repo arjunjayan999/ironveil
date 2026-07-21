53dbaaac67aa89a9d75e7a144f33383b19dd0051
Arjun Jayan <arjunjayan999@proton.me>
2026-07-21 17:39:53 +0000

    Add registration endpoint in backend

--- apps/backend/src/routes/auth.ts
+++ apps/backend/src/routes/auth.ts
@@ -1,12 +1,19 @@
 import argon2 from "argon2";
 import type { FastifyPluginAsync } from "fastify";
+import { DatabaseError } from "pg";
 import { z } from "zod";
-import { findUserByUsername } from "../db/queries/users.js";
+import { createUser, findUserByUsername } from "../db/queries/users.js";
 import { authenticate } from "../middleware/auth.js";
 
 const loginBodySchema = z.object({
 	username: z.string().min(1),
 	password: z.string().min(1),
+});
+
+const registerBodySchema = z.object({
+	username: z.string().min(3).max(32),
+	email: z.email(),
+	password: z.string().min(8),
 });
 
 export const authRoutes: FastifyPluginAsync = async (fastify) => {
@@ -39,6 +46,59 @@ export const authRoutes: FastifyPluginAsync = async (fastify) => {
 			token,
 			user: { id: user.id, username: user.username, email: user.email },
 		});
+	});
+
+	fastify.post("/register", async (request, reply) => {
+		const result = registerBodySchema.safeParse(request.body);
+
+		if (!result.success) {
+			return reply.status(400).send({
+				error: "Bad Request",
+				message: "Invalid request body",
+			});
+		}
+
+		const { username, email, password } = result.data;
+
+		const passwordHash = await argon2.hash(password);
+		try {
+			const user = await createUser(username, email, passwordHash);
+
+			const token = fastify.jwt.sign(
+				{
+					sub: user.id,
+					username: user.username,
+				},
+				{
+					expiresIn: "8h",
+				},
+			);
+
+			return reply.status(201).send({
+				token,
+				user,
+			});
+		} catch (err: unknown) {
+			if (err instanceof DatabaseError) {
+				if (err.code === "23505") {
+					switch (err.constraint) {
+						case "users_username_key":
+							return reply.status(409).send({
+								error: "Conflict",
+								message: "Username already exists",
+							});
+
+						case "users_email_key":
+							return reply.status(409).send({
+								error: "Conflict",
+								message: "Email already exists",
+							});
+					}
+				}
+			}
+
+			throw err;
+		}
 	});
 
 	fastify.get("/me", { preHandler: [authenticate] }, async (request, reply) => {
